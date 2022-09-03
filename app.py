b2fe0e8cc84cb7caae1c45d6218fd098565177eb
BlueBlue <49936395+BlueBlue21@users.noreply.github.com>
2022-09-04 02:00:30 +0900

    📘

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-def hate(str):
-  return f"i hate {str}"
+# Me love python!
 
-print(hate("python"))
+def hate(str):
+  return f"I hate {str}"
+
+def main():
+  print(hate("python"))
+  print("But me love python!")
+
+if __name__ == "__main__":
+  main()
